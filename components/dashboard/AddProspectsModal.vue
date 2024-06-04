f4f0f1db4224cfd53e9308079330618b1bed701e
emmyalexjnr <emmyalexjnr@gmail.com>
2024-06-04 16:29:40 +0100

    - working on prospects

---
+++ components/dashboard/AddProspectsModal.vue
@@ -0,0 +1,90 @@
+<template>
+    <DialogContent>
+        <DialogHeader>
+            <DialogTitle>Add Prospect</DialogTitle>
+            
+            <div class="mt-4">
+                <form class=" space-y-6" @submit="onSubmit">
+                    <FormField v-slot="{ componentField }" name="username">
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input type="text" placeholder="name" v-bind="componentField" />
+                            </FormControl>
+                            
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+                    <FormField v-slot="{ componentField }" name="username">
+                        <FormItem>
+                            <FormLabel>Email</FormLabel>
+                            <FormControl>
+                                <Input type="email" placeholder="email" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+                    <FormField v-slot="{ componentField }" name="username">
+                        <FormItem>
+                            <FormLabel>Phone Number</FormLabel>
+                            <FormControl>
+                                <Input type="text" placeholder="phone number" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+                    <FormField v-slot="{ componentField }" name="username">
+                        <FormItem>
+                            <FormLabel>Description</FormLabel>
+                            <FormControl>
+                                <Input type="text" placeholder="description" v-bind="componentField" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    </FormField>
+                    <Button type="submit">
+                        Submit
+                    </Button>
+                </form>
+                
+            </div>
+        </DialogHeader>
+
+        <DialogFooter>
+            
+        </DialogFooter>
+    </DialogContent>
+</template>
+
+<script setup>
+import {
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from '@/components/ui/dialog';
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from '@/components/ui/form';
+import { toTypedSchema } from '@vee-validate/zod';
+import { useForm } from 'vee-validate';
+import * as z from 'zod';
+
+const formSchema = toTypedSchema(z.object({
+    username: z.string().min(2).max(50),
+}))
+
+const form = useForm({
+    validationSchema: formSchema,
+})
+
+const onSubmit = form.handleSubmit((values) => {
+    console.log('Form submitted!', values)
+})
+</script>
+
+<style></style>
---
+++ pages/dashboard/prospects.vue
@@ -0,0 +1,195 @@
+<template>
+    <div>
+        <div class="mb-4">
+            <h1 class="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">All Prospects</h1>
+        </div>
+        <div class="sm:flex mb-4">
+            <div class="items-center hidden mb-3 sm:flex sm:divide-x sm:divide-gray-100 sm:mb-0 dark:divide-gray-700">
+                <form class="lg:pr-3" action="#" method="GET">
+                    <label for="users-search" class="sr-only">Search</label>
+                    <div class="relative mt-1 lg:w-64 xl:w-96">
+                        <input type="text" name="email" id="users-search"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Search for users">
+                    </div>
+                </form>
+            </div>
+            <div class="flex items-center ml-auto space-x-2 sm:space-x-3">
+                <Button type="button" class="py-2 bg-blue-800">
+                    <svg class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    <NuxtLink to="/dashboard/send-message">
+                        Send Messages
+                    </NuxtLink>
+                </Button>
+                <Dialog>
+                    <DialogTrigger>
+                        <Button type="button" data-modal-target="add-user-modal" data-modal-toggle="add-user-modal"
+                            class="py-2 bg-blue-800">
+                            <svg class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                            Add Prospect
+                        </Button>
+                    </DialogTrigger>
+                    <AddProspectsModal />
+                </Dialog>
+
+                <a href="#"
+                    class="inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 sm:w-auto dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">
+                    <svg class="w-5 h-5 mr-2 -ml-1" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    Export
+                </a>
+            </div>
+        </div>
+        <Table>
+            <thead class="bg-gray-100 dark:bg-gray-700"></thead>
+            <TableHeader class="bg-gray-100 dark:bg-gray-700">
+                <TableRow>
+                    <TableHead>
+
+                    </TableHead>
+                    <TableHead>
+                        Avatar
+                    </TableHead>
+                    <TableHead>
+                        Name
+                    </TableHead>
+
+                    <TableHead>
+                        Description
+                    </TableHead>
+                    <TableHead>Phone number</TableHead>
+                    <TableHead class="text-right">
+                        Action
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                    <TableCell>
+                        <Checkbox id="terms" />
+                    </TableCell>
+                    <TableCell>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </TableCell>
+                    <TableCell class="font-medium">
+                        <div class="text-base font-semibold text-gray-900 dark:text-white">Mr Ahmed Danduma</div>
+                        <div class="text-sm font-normal text-gray-500 dark:text-gray-400">abduldanduma@gmail.com</div>
+                    </TableCell>
+
+                    <TableCell>thsis allshds asksalas </TableCell>
+                    <TableCell>+2347689878498</TableCell>
+                    <TableCell class="text-right">
+                        <div class="flex gap-3 justify-end">
+
+                            <Button class=" bg-blue-800">
+                                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z">
+                                    </path>
+                                    <path fill-rule="evenodd"
+                                        d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                                Edit
+                            </Button>
+                            <Button class=" bg-red-500">
+                                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                                Delete
+                            </Button>
+                        </div>
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell>
+                        <Checkbox id="terms" />
+                    </TableCell>
+                    <TableCell>
+                        <Avatar>
+                            <AvatarImage src="https://github.com/radix-vue.png" alt="@radix-vue" />
+                            <AvatarFallback>CN</AvatarFallback>
+                        </Avatar>
+                    </TableCell>
+                    <TableCell class="font-medium">
+                        <div class="text-base font-semibold text-gray-900 dark:text-white">Mr Ahmed Danduma</div>
+                        <div class="text-sm font-normal text-gray-500 dark:text-gray-400">abduldanduma@gmail.com</div>
+                    </TableCell>
+
+                    <TableCell>ajhsj ajsjsk</TableCell>
+                    <TableCell>+2347689878498</TableCell>
+                    <TableCell class="text-right">
+                        <div class="flex gap-3 justify-end">
+
+                            <Button class=" bg-blue-800">
+                                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z">
+                                    </path>
+                                    <path fill-rule="evenodd"
+                                        d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                                Edit
+                            </Button>
+                            <Button class=" bg-red-500">
+                                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                        clip-rule="evenodd"></path>
+                                </svg>
+                                Delete
+                            </Button>
+                        </div>
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+
+        
+    </div>
+</template>
+
+<script setup>
+import {
+    Dialog,
+    DialogTrigger
+} from '@/components/ui/dialog';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow
+} from '@/components/ui/table';
+import AddProspectsModal from '~/components/dashboard/AddProspectsModal.vue';
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+definePageMeta({
+    layout: 'dashboard'
+})
+</script>
